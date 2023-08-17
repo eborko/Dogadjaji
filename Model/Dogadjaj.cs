f08fe5ca5f0670fb8adc228502303d7f280110ee
Borko Djurovic <borko.djurovic@hotmail.com>
2023-08-17 22:57:52 +0200

    Kreiranje klasa Model sloja

---
+++ Model/Dogadjaj.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Microsoft.VisualBasic;
+
+namespace Dogadjaji.Model
+{
+    class Dogadjaj : INotifyPropertyChanged
+    {
+        #region Fields
+        private Guid _id;
+        private string _naziv;
+        private Grad _gradOdrzavanja;
+        private DateAndTime _datumIVrijemePocetka;
+        private int _trajanjeUMinutima;
+        #endregion
+
+        #region Constructors
+        public Dogadjaj(string naziv, Grad gradOdrzavanja, DateAndTime datumIVrijemePocetka, int trajanjeUMinutima)
+        {
+            _naziv = naziv;
+            _id = Guid.NewGuid();
+            _gradOdrzavanja = gradOdrzavanja;
+            _datumIVrijemePocetka = datumIVrijemePocetka;
+            _trajanjeUMinutima = trajanjeUMinutima;
+        }
+        #endregion
+
+        /// <summary>
+        /// Dogadjaj (misli se na jezicki Event u C# a ne na nas Dogadjaj u Modelu)
+        /// uvezen je iz INotifyPropertyChanged interfejsa
+        /// </summary>
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        /// <summary>
+        /// Uvijek se poziva prilikom promjene nekog od Property-ja
+        /// </summary>
+        /// <param name="propertyName">Naziv Property-ja</param>
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #region Properties
+        public Guid Id { get { return _id; } }
+
+        public string Naziv
+        {
+            get { return _naziv; }
+            set
+            {
+                if (_naziv != value) { _naziv = value; NotifyPropertyChanged(); }
+            }
+        }
+
+        public Grad GradOdrzavanja
+        {
+            get { return GradOdrzavanja; }
+            set
+            {
+                if (value != null && value.Id != _gradOdrzavanja.Id)
+                {
+                    _gradOdrzavanja = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public DateAndTime DatumIVrijemePocetka 
+        {
+            get { return _datumIVrijemePocetka; }
+            set
+            {
+                if (value != null && !DateAndTime.Equals(value, _datumIVrijemePocetka))
+                {
+                    _datumIVrijemePocetka = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public int TrajanjeUMinutima 
+        {
+            get { return _trajanjeUMinutima; } 
+            set
+            {
+                if (_trajanjeUMinutima != value)
+                {
+                    _trajanjeUMinutima = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+        #endregion
+    }
+}
---
+++ Model/Grad.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Dogadjaji.Model
+{
+    class Grad : INotifyPropertyChanged
+    {
+        #region Fields
+        private Guid _id;
+        private string _naziv;
+        private ISet<Dogadjaj> _dogadjaji;
+        #endregion
+
+        #region Constructors
+        public Grad(string naziv)
+        {
+            _id = Guid.NewGuid();
+            _naziv = naziv;
+            _dogadjaji = new HashSet<Dogadjaj>();
+        }
+        #endregion
+
+        /// <summary>
+        /// Dogadjaj (misli se na jezicki Event u C# a ne na nas Dogadjaj u Modelu)
+        /// uvezen je iz INotifyPropertyChanged interfejsa
+        /// </summary>
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        /// <summary>
+        /// Uvijek se poziva prilikom promjene nekog od Property-ja
+        /// </summary>
+        /// <param name="propertyName">Naziv Property-ja</param>
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #region Properties
+        public Guid Id { get { return _id; } }
+
+        public string Naziv
+        {
+            get { return _naziv; }
+            set
+            {
+                if (value != null && !value.Equals(_naziv, StringComparison.InvariantCultureIgnoreCase))
+                { 
+                    _naziv = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public ISet<Dogadjaj> Dogadjaji { get { return _dogadjaji; } }
+        #endregion
+    }
+}
